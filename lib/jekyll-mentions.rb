7cfd7cd4b5a23ede898cd6b368fd59e5c67a26b5
Ben Balter <ben.balter@github.com>
2014-01-22 10:22:34 -0500

    monkeypatch html pipeline to handle arbitrary strings

--- lib/jekyll-mentions.rb
+++ lib/jekyll-mentions.rb
@@ -15,9 +15,23 @@ module Jekyll
     end
 
     def mentionify(page)
-      filter = HTML::Pipeline::MentionFilter.new("<#{TAG}>#{page.content}</#{TAG}>", { :base_url => URL })
-      page.content = filter.call.search(TAG).children.to_xml
+      filter = HTML::Pipeline::MentionFilter.new(page.content, { :base_url => URL })
+      page.content = filter.call
     end
 
+  end
+end
+
+module HTML
+  class Pipeline
+    class MentionFilter < Filter
+      def call
+        if html.include?('@')
+          mention_link_filter(html, base_url, info_url)
+        else
+          html
+        end
+      end
+    end
   end
 end
--- test/test_jekyll_mentions.rb
+++ test/test_jekyll_mentions.rb
@@ -8,7 +8,7 @@ class TestJekyllMentions < Test::Unit::TestCase
     @page = Jekyll::Page.new(@site, File.expand_path("../../", __FILE__), "", "README.md")
     @page.instance_variable_set "@content", "test @test test"
     @site.pages.push @page
-    @mention = "test <a href=\"https://github.com/test\" class=\"user-mention\">@test</a> test"
+    @mention = "test <a href='https://github.com/test' class='user-mention'>@test</a> test"
   end
 
   should "replace @mention with link" do
@@ -27,6 +27,14 @@ class TestJekyllMentions < Test::Unit::TestCase
 
     @mentions.mentionify page
     assert_equal "#{@mention}<a href=\"{{ test }}\">test</a>", page.content
+  end
+
+  should "not mangle markdown" do
+    page = Jekyll::Page.new(@site, File.expand_path("../../", __FILE__), "", "README.md")
+    page.instance_variable_set "@content", "test @test test\n> test"
+
+    @mentions.mentionify page
+    assert_equal "#{@mention}\n> test", page.content
   end
 
 end
