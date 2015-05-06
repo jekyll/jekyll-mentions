7b3dffe08bf87c1166e03d4739c19de3a14dfa04
Parker Moore <parkrmoore@gmail.com>
2015-05-05 20:39:10 -0700

    mentions: make it a converter instead of a generator

--- lib/jekyll-mentions.rb
+++ lib/jekyll-mentions.rb
@@ -2,28 +2,36 @@ require 'jekyll'
 require 'html/pipeline'
 
 module Jekyll
-  class Mentions < Jekyll::Generator
+  class Mentions < Jekyll::Converter
+    MENTIONABLE_EXTS = %w{.html .htm .md .markdown .mkdn .mkd .textile}
+
     safe true
+    priority :low
+
+    attr_reader :filter
 
     DEFAULT_URL = "https://github.com"
 
     def initialize(config = Hash.new)
-      @filter = HTML::Pipeline::MentionFilter.new(nil, {:base_url => base_url(config['jekyll-mentions']) })
+      @filter = HTML::Pipeline::MentionFilter.new(nil, {
+        :base_url => base_url(config['jekyll-mentions'])
+      })
     end
 
-    def generate(site)
-      site.pages.each { |page| mentionify page if html_page?(page) }
-      site.posts.each { |post| mentionify post }
-      site.docs_to_write.each { |doc| mentionify doc }
+    def matches(ext)
+      MENTIONABLE_EXTS.include?(ext.downcase)
     end
 
-    def mentionify(page)
-      return unless page.content.include?('@')
-      page.content = @filter.mention_link_filter(page.content)
+    def output_ext(*)
+      ".html".freeze
     end
 
-    def html_page?(page)
-      page.html? || page.url.end_with?('/')
+    def convert(content)
+      if content.include?('@')
+        filter.mention_link_filter(content)
+      else
+        content
+      end
     end
 
     def base_url(configs)
